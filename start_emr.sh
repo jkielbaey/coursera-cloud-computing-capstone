f107eaa4276f8c97007747a3e3a2a69999653d0f
Johan Kielbaey <johan@kielbaey-oliveros.eu>
2017-08-02 22:12:22 +0200

    Fix command to start emr cluster.

--- start_emr.sh
+++ start_emr.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-aws emr create-default-roles
+$ aws emr create-default-roles
 
-aws emr create-cluster \
-  --name 'capstone-cluster' \
-  --region eu-west-1 \
-  --release-label emr-5.7.0 \
-  --use-default-roles \
-  --tags 'capstone=true' 'Name=capstone-cluster' \
-  --no-termination-protected \
-  --log-uri 's3://jkielbaey-capstone-eu-west-1/EMR/' \
+$ aws emr create-cluster \
+    --name 'capstone-cluster-20170802' \
+    --region eu-west-1 \
+    --release-label emr-5.7.0 \
+    --use-default-roles \
+    --tags 'capstone=true' \
+    --no-termination-protected \
   --no-enable-debugging \
-  --applications Name=Hadoop Name=Hive Name=Pig Name=Hue \
-  --auto-scaling-role EMR_AutoScaling_DefaultRole \
-  --ec2-attributes 'KeyName=id_rsa.jkielbaey-20160502,SubnetId=subnet-7ce0f425,AdditionalSlaveSecurityGroups=[sg-47e7273f],EmrManagedSlaveSecurityGroup=sg-329d334a,EmrManagedMasterSecurityGroup=sg-b09f31c8,AdditionalMasterSecurityGroups=[sg-47e7273f]' \
-  --steps file://load_s3_hdfs_hive.json \
-  --configurations file://emr-configurations.json \
-  --instance-groups \
-      'InstanceGroupType=MASTER,InstanceCount=1,InstanceType=m4.large' \
-      'InstanceGroupType=CORE,InstanceCount=5,InstanceType=c4.2xlarge,EbsConfiguration={EbsOptimized=true,EbsBlockDeviceConfigs=[{VolumeSpecification={VolumeType=gp2,SizeInGB=100}}]}' \
-  --scale-down-behavior TERMINATE_AT_INSTANCE_HOUR
+    --auto-scaling-role EMR_AutoScaling_DefaultRole \
+    --applications Name=Hadoop Name=Hive Name=Pig Name=Hue \
+    --ec2-attributes 'KeyName=id_rsa.jkielbaey-20160502,AdditionalSlaveSecurityGroups=[sg-47e7273f],SubnetId=subnet-7ce0f425,EmrManagedSlaveSecurityGroup=sg-988b31e0,EmrManagedMasterSecurityGroup=sg-3b952f43,AdditionalMasterSecurityGroups=[sg-47e7273f]' \
+    --log-uri 's3n://jkielbaey-capstone-eu-west-1/EMR/' \
+    --instance-groups \
+        'InstanceGroupType=MASTER,Name=Master,InstanceCount=1,InstanceType=m4.large,EbsConfiguration={EbsOptimized=true,EbsBlockDeviceConfigs=[{VolumeSpecification={VolumeType=gp2,SizeInGB=32},VolumesPerInstance=1}]}' \
+        'InstanceGroupType=CORE,Name=Core,InstanceCount=5,InstanceType=c4.2xlarge,EbsConfiguration={EbsOptimized=true,EbsBlockDeviceConfigs=[{VolumeSpecification={VolumeType=gp2,SizeInGB=100},VolumesPerInstance=1}]}' \
+    --configurations file://emr-configurations.json \
+    --scale-down-behavior TERMINATE_AT_INSTANCE_HOUR
+
+$ aws emr add-steps --cluster-id j-V5SERYYW830T --steps file://load_s3_hdfs_hive.json
