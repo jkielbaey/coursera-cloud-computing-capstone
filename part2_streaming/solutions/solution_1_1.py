567715eecb365f27b4bd3d1971b6e49b9c643f08
Johan Kielbaey <johan@kielbaey-oliveros.eu>
2017-08-19 18:39:24 +0200

    Improve solution by doing sorting before pulling results back to the driver.

--- part2_streaming/solutions/solution_1_1.py
+++ part2_streaming/solutions/solution_1_1.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
 
 import sys
-import datetime
-
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
@@ -22,27 +20,21 @@ if __name__ == '__main__':
         items = line.split(",")
         return {LINE_FIELDS[i]: items[i] for i in range(0, len(LINE_FIELDS))}
 
-    def do_map(event):
-        return ((event['Origin'], 1), (event['Dest'], 1))
-
-    def do_reduce(a, b):
-        return a + b
-
     def do_update(new_values, rolling_sum):
         return sum(new_values) + (rolling_sum or 0)
 
-    def print_results(rdd):
-        print('--- %s - Results ---' % datetime.datetime.now())
-        ordered_rdd = rdd.sortBy(lambda x: x[1], ascending=False).take(10)
-        for record in ordered_rdd:
+    def print_results(time, rdd):
+        print("--- %s ---" % str(time))
+        for record in rdd.take(10):
             print(','.join([record[0], str(record[1])]))
 
     lines = ssc.textFileStream(sys.argv[1])
-    process = lines.map(convert_lines_to_dict) \
-                   .flatMap(do_map) \
-                   .reduceByKey(do_reduce) \
-                   .updateStateByKey(do_update)
-    process.foreachRDD(print_results)
+    parsed = lines.map(convert_lines_to_dict)
+    processed = parsed.flatMap(lambda r: ((r['Origin'], 1), (r['Dest'], 1))) \
+                      .reduceByKey(lambda a, b: a + b) \
+                      .updateStateByKey(do_update)
+    ordered = processed.transform(lambda rdd: rdd.sortBy(lambda x: x[1], ascending=False))
+    ordered.foreachRDD(print_results)
 
     ssc.start()
     ssc.awaitTermination()
